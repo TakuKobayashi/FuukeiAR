6e6d8348d1619cc634f66055686f8b8e077cf382
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-11-05 12:01:22 +0900

    とりあえずシェア機能をオラッと入れてみた

--- app/src/main/java/kobayashi/taku/com/fuukeiar/PreviewActivity.java
+++ app/src/main/java/kobayashi/taku/com/fuukeiar/PreviewActivity.java
@@ -1,37 +1,17 @@
 package kobayashi.taku.com.fuukeiar;
 
 import android.app.Activity;
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.PointF;
-import android.graphics.RectF;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.util.SparseArray;
-import android.view.Surface;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
+import android.support.v4.app.ShareCompat;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 
 import com.bumptech.glide.Glide;
-import com.google.android.gms.vision.MultiProcessor;
-import com.google.android.gms.vision.Tracker;
-import com.google.android.gms.vision.face.Face;
-import com.google.android.gms.vision.face.FaceDetector;
 
 import java.io.File;
-import java.util.ArrayList;
 
 public class PreviewActivity extends Activity {
     @Override
@@ -45,8 +25,50 @@ public class PreviewActivity extends Activity {
 
         setContentView(R.layout.take_picture_preview);
 
-        ImageView previewImage = (ImageView) findViewById(R.id.taku_picture_view);
+        ImageView previewImage = (ImageView) findViewById(R.id.take_picture_view);
         Glide.with(this).load(Uri.fromFile(new File(getIntent().getStringExtra("saveImagePath")))).into(previewImage);
+
+        ImageView facebookButton = (ImageView) findViewById(R.id.facebook_image_button);
+        facebookButton.setImageBitmap(Util.loadImageFromAsset(this, "icons/pct_facebook.png"));
+        facebookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        ImageView tweetButton = (ImageView) findViewById(R.id.tweet_image_button);
+        tweetButton.setImageBitmap(Util.loadImageFromAsset(this, "icons/pct_twitter.png"));
+        tweetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        ImageView lineButton = (ImageView) findViewById(R.id.line_image_button);
+        lineButton.setImageBitmap(Util.loadImageFromAsset(this, "icons/pct_line.png"));
+        lineButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ShareCompat.IntentBuilder builder = ShareCompat.IntentBuilder.from(PreviewActivity.this);
+                builder.setChooserTitle(getString(R.string.app_name));
+                builder.setText("#ma_2017");
+                builder.setType("image/png");
+                builder.addStream(Uri.fromFile(new File(getIntent().getStringExtra("saveImagePath"))));
+                builder.startChooser();
+            }
+        });
+
+        ImageView backButton = (ImageView) findViewById(R.id.back_image_button);
+        backButton.setImageBitmap(Util.loadImageFromAsset(this, "pct_back.png"));
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     @Override
@@ -67,6 +89,10 @@ public class PreviewActivity extends Activity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Util.releaseImageView((ImageView) findViewById(R.id.taku_picture_view));
+        Util.releaseImageView((ImageView) findViewById(R.id.take_picture_view));
+        Util.releaseImageView((ImageView) findViewById(R.id.facebook_image_button));
+        Util.releaseImageView((ImageView) findViewById(R.id.tweet_image_button));
+        Util.releaseImageView((ImageView) findViewById(R.id.line_image_button));
+        Util.releaseImageView((ImageView) findViewById(R.id.back_image_button));
     }
 }
